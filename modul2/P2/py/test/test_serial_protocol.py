c017ffebbf970eff8a333b927e4031a6750dac14
Diez B. Roggisch <deets@web.de>
2021-05-30 18:26:52 +0200

    Implement the R command
    This allows to set the samplerate

--- modul2/P2/py/test/test_serial_protocol.py
+++ modul2/P2/py/test/test_serial_protocol.py
@@ -4,7 +4,7 @@ import pytest
 import operator
 from functools import reduce
 
-PORT = "/dev/serial/by-id/usb-Parallax_Inc_Propeller_P2-EVAL_PLX4L28HW-if00-port0"
+PORT = "/dev/serial/by-id/usb-FTDI_USB__-__Serial-if00-port0"
 BAUD = 115200
 
 
@@ -27,4 +27,9 @@ def conn():
 
 def test_ping(conn):
     conn.write(rqads("P"))
+    print(conn.readline())
+
+
+def test_rate(conn):
+    conn.write(rqads("R$F0"))
     print(conn.readline())
