15987aec37b1db375a2033d074c9f0f2544ad44d
Diez B. Roggisch <deets@web.de>
2021-07-04 12:24:36 +0200

    Stream the data file line format over serial

---
+++ modul2/P2/py/read-sequential-files.py
@@ -0,0 +1,26 @@
+# Copyright: 2021, Diez B. Roggisch . All rights reserved.
+import sys
+import serial
+from itertools import count
+
+PORT = "/dev/serial/by-id/usb-FTDI_USB__-__Serial-if00-port0"
+BAUD = 1_000_000
+
+
+def main():
+    conn = serial.Serial(PORT, BAUD)
+    for number in count(1):
+        filename = f"/tmp/RQADS{number:03x}.DAT"
+        print(f"\nstreaming to {filename}")
+        with open(filename, "wb") as outf:
+            while True:
+                line = conn.readline()
+                if line.strip() == b"done":
+                    break
+                outf.write(line)
+                print(".", end="")
+                sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
